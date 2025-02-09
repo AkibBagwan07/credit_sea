2d4b0a5e852bcb4c257eabdc49fc09696030669c
Akib Bagwan <127624748+AkibBagwan07@users.noreply.github.com>
2025-02-09 17:06:46 +0530

    corrected link api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://credit-sea-backend-om6f.onrender.com/api";
+const API_URL = "https://credit-sea-backend-om6f.onrender.com";
 
 export const uploadFile = async (file) => {
   const formData = new FormData();
